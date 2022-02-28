4dd5a17a02483db6b06cbfe75490d8072cd1c977
Malkaris <Malkaris95@gmail.com>
2022-02-28 16:50:47 +0300

    done

--- 01_simple_sum_from_list.py
+++ 01_simple_sum_from_list.py
@@ -1,2 +1,14 @@
 # Создайте функцию, которая будет считать сумму чисел в случано сгенерированном массиве длиной 10
 # и выводить результат в консоль
+
+from random import randint
+lst = [randint(1,10) for i in range(10)]
+
+def summ_lst(list) -> int:
+    a = 0
+    for i in list:
+        a+=i
+    return a
+
+print(lst)
+print(summ_lst(lst))
